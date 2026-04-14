60f3b82bd34e46341d5a28071646a5a96860a324
William Acuña <unknow.citizen@protonmail.com>
2026-04-13 22:08:20 -0500

    06 Sidebar

---
+++ src/modules/dashboard/ui/components/dashboard-sidebar.tsx
@@ -0,0 +1,123 @@
+"use client"
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem
+
+} from "@/components/ui/sidebar"
+import { cn } from "@/lib/utils";
+import { Separator } from "@/components/ui/separator";
+import { BotIcon, StarIcon, VideoIcon } from "lucide-react"
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { DashboardUserButton } from "./dashboard-User-Button";
+
+
+
+const firstSection = [
+    {
+        icon:VideoIcon,
+        label:'Safe Space',
+        href:'/meetings'
+    },
+    {
+        icon: BotIcon,
+        label:"Agentes",
+        href:"/agents"
+    },    
+];
+
+const secondSection = [
+    {
+        icon:StarIcon,
+        label:'Premium',
+        href:'/upgrade'
+    }   
+];
+
+export const DashBoardSideBar = () =>{
+    const pathname = usePathname();
+    
+    return(
+        <Sidebar>
+            <SidebarHeader className="text-sidebar-accent-foreground">
+                <Link href="/" className="flex items-center gap-2 px-2 pt2">
+                    <Image src="/logo.svg" height={36} width={36} alt="RefugIa"/>
+                    <p className="text-2xl font-semibold">Refug.ia</p>
+                </Link>
+            </SidebarHeader>
+            <div className="px-4 py-2">
+                <Separator className="opacity-10 text-[#5D6B68]"/>
+            </div>
+            <SidebarContent>
+                <SidebarGroup>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            {firstSection.map((item)=>(
+                                <SidebarMenuItem key={item.href}>
+                                    <SidebarMenuButton 
+                                        asChild
+                                        className={cn(
+                                        "h-10 hover:bg-linear-to-r/oklch border border-transparent hover:border-[#5D6B68]/10 from-sidebar-accent from-5% via-30% via-sidebar/50 to-sidebar/50",
+                                        pathname === item.href && "bg-linear-to-r/oklch border-[#5d6b68]/10"
+                                    )}
+                                        isActive={pathname === item.href}
+                                    >
+                                        <Link href={item.href}>
+                                            <item.icon className="size-5"/>
+                                            <span className="text-sm font-medium tracking-tight">
+                                                {item.label}
+                                            </span>
+                                        </Link>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            ))}
+
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+                 <div className="px-4 py-2">
+                    <Separator className="opacity-10 text-[#5D6B68]"/>
+                </div>
+                <SidebarGroup>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            {secondSection.map((item)=>(
+                                <SidebarMenuItem key={item.href}>
+                                    <SidebarMenuButton 
+                                        asChild
+                                        className={cn(
+                                        "h-10 hover:bg-linear-to-r/oklch border border-transparent hover:border-[#5D6B68]/10 from-sidebar-accent from-5% via-30% via-sidebar/50 to-sidebar/50",
+                                        pathname === item.href && "bg-linear-to-r/oklch border-[#5d6b68]/10"
+                                    )}
+                                        isActive={pathname === item.href}
+                                    >
+                                        <Link href={item.href}>
+                                            <item.icon className="size-5"/>
+                                            <span className="text-sm font-medium tracking-tight">
+                                                {item.label}
+                                            </span>
+                                        </Link>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            ))}
+
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+            </SidebarContent>
+            <SidebarFooter className="text-white">
+
+                <DashboardUserButton/>
+
+            </SidebarFooter>
+        </Sidebar>
+    )
+}
