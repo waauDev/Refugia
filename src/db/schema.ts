88ae955356456689a26eeee8d3f4ae4331b13520
William Acuña <unknow.citizen@protonmail.com>
2026-05-04 22:17:48 -0500

    09 -agents

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,6 @@
 
 import { pgTable, text, timestamp, boolean } from "drizzle-orm/pg-core";
+import {nanoid} from "nanoid";
 
 export const user = pgTable("user", {
   id: text("id").primaryKey(),
@@ -60,3 +61,17 @@ export const verification = pgTable("verification", {
     .$onUpdate(() => /* @__PURE__ */ new Date())
     .notNull(),
 });
+
+export const agents = pgTable("agents",{
+  id:text("id")
+    .primaryKey()
+    .$defaultFn(()=> nanoid()),
+  name: text("name").notNull(),
+  userId:text("user_id")
+    .notNull()
+    .references(()=> user.id, {onDelete:"cascade"}),
+    instructions: text("instructions").notNull(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    updatedAt: timestamp("updated_at").notNull().defaultNow(),
+});
+
