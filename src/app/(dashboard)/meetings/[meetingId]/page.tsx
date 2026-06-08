da60bbcf32ac263ee851a7b0404ee58837804b97
William Acuña <unknow.citizen@protonmail.com>
2026-06-08 16:21:57 -0500

    16 feat: add meetings functionality with CRUD operations and UI components

---
+++ src/app/(dashboard)/meetings/[meetingId]/page.tsx
@@ -0,0 +1,9 @@
+const Page = () =>{
+    return(
+        <div>
+            Meetings id
+        </div>
+    )
+}
+
+export default Page
---
+++ src/modules/meetings/ui/meetings-view.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import { ErrorState } from "@/components/error-state";
+import { LoadingState } from "@/components/loading-state";
+import { useTRPC } from "@/trpc/client"
+import {  useSuspenseQuery } from "@tanstack/react-query"
+
+export const MeetingsView = () =>{
+
+    const trpc = useTRPC();
+    const {data} = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}))
+
+    return (
+        <div>
+            {JSON.stringify(data?.items)}
+        </div>
+    )
+}
+
+export const MeetingsViewLoading = ()=>{
+    return(
+        <LoadingState 
+            title="Cargando" 
+            description="Esto puede tardar unos segundos..."
+            />
+    )
+}
+
+export const MeetingsViewError = () =>{
+ return(
+        <ErrorState 
+            title="No ha sido posible cargar la reunion"
+            description="Por favor intentar mas tarde..."
+        />
+    )
+}
+
